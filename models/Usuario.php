be5552b7fe3fc2128035a4dcdc51d33c5748944b
Frankd11366 <capurivas@gmail.com>
2024-08-20 01:55:25 -0600

    login

--- models/Usuario.php
+++ models/Usuario.php
@@ -6,7 +6,7 @@ class Usuario extends ActiveRecord
 {
     protected static $tabla = 'usuario';
     protected static $idTabla = 'usu_id';
-    protected static $columnasDB = ['usu_id', 'usu_nombre', 'usu_catalogo', 'usu_password', 'usu_situacion'];
+    protected static $columnasDB = ['usu_nombre', 'usu_catalogo', 'usu_password', 'usu_situacion'];
 
     public $usu_id;
     public $usu_nombre;
@@ -19,14 +19,22 @@ class Usuario extends ActiveRecord
     {
         $this->usu_id = $args['usu_id'] ?? null;
         $this->usu_nombre = $args['usu_nombre'] ?? '';
-        $this->usu_catalogo = $args['usu_catalogo'] ?? 0;
+        $this->usu_catalogo = $args['usu_catalogo'] ?? '';
         $this->usu_password = $args['usu_password'] ?? '';
         $this->usu_situacion = $args['usu_situacion'] ?? 1;
     }
 
-    public static function obtenerRolconQuery()
+    public function validarUsuarioExistente(): bool
     {
-        $sql = "SELECT * FROM usuario where usu_situacion = 1";
-        return self::fetchArray($sql);
+        $sql = "SELECT * FROM usuario where usu_catalogo = $this->usu_catalogo";
+        $resultado = static::fetchArray($sql);
+        return $resultado ? true : false;
+    }
+
+    public function getUsuarioExistente(): array
+    {
+        $sql = "SELECT usu_id,usu_nombre, usu_password, usu_catalogo, rol_nombre_ct, rol_nombre from permiso inner join usuario on permiso_usuario = usu_id inner join rol on rol_id = permiso_rol inner join aplicacion on rol_app = app_id where usu_catalogo = $this->usu_catalogo";
+        $resultado = static::fetchFirst($sql);
+        return $resultado;
     }
 }
